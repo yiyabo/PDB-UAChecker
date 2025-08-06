caa5ff343b9c67be9a1d737973e0c4a9f4890973
Yiyabo <wxxpublic@outlook.com>
2025-08-06 15:05:08 +0800

    feat : clean the unnecessary

--- src/search/__init__.py
+++
@@ -1,25 +0,0 @@
-#!/usr/bin/env python3
-"""
-"!W
-Ð›Œt„(úx"Ÿý
-"""
-
-from .engine import ScalableSearchEngine
-from .indexing import IndexManager
-from .strategies import (
-    ResidueNameMatcher,
-    BasicFingerprintMatcher,
-    MolecularFormulaSearcher,
-    MolecularWeightSearcher,
-    FeatureSearcher
-)
-
-__all__ = [
-    'ScalableSearchEngine',
-    'IndexManager',
-    'ResidueNameMatcher',
-    'BasicFingerprintMatcher',
-    'MolecularFormulaSearcher',
-    'MolecularWeightSearcher',
-    'FeatureSearcher'
-]
